1dd710647cc5e7d3ff680fbdb4c595aafceda6eb
Zach Leffke <zleffke@vt.edu>
2019-12-27 12:59:26 -0500

    Parameter updates...nothing major.

--- v2.0/dev/burst_rx_gmsk9600_ax25_playback.py
+++ v2.0/dev/burst_rx_gmsk9600_ax25_playback.py
@@ -172,7 +172,7 @@ class burst_rx_gmsk9600_ax25_playback(gr.top_block, Qt.QWidget):
         for c in range(0, 6):
             self.main_tab_grid_layout_1.setColumnStretch(c, 1)
 
-        self.sigmf_source_0 = gr_sigmf.source('/vtgs/captures/vcc/uva_keep/VCC_VTGS_20191123_043458.sigmf-data', "cf32" + ("_le" if sys.byteorder == "little" else "_be"), False)
+        self.sigmf_source_0 = gr_sigmf.source('/vtgs/captures/vcc/VCC_VTGS_20191227_155437.sigmf-data', "cf32" + ("_le" if sys.byteorder == "little" else "_be"), False)
         self._rx_gain_tool_bar = Qt.QToolBar(self)
         self._rx_gain_tool_bar.addWidget(Qt.QLabel('RX Gain'+": "))
         self._rx_gain_line_edit = Qt.QLineEdit(str(self.rx_gain))
@@ -295,7 +295,7 @@ class burst_rx_gmsk9600_ax25_playback(gr.top_block, Qt.QWidget):
         	2 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.010)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0.set_y_axis(-10, 10)
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
